1b00064110d038970ba0d0c81537f898b65f7760
swkidd <kidds@acm.org>
2019-02-13 09:41:38 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
 import './App.css';
+import Tab from './components/Tab.tsx'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [componentList, setComponentList] = useState([])
+  return (
+    <Fragment>
+      <header>
+        <nav className="nav">SOME COOL APP</nav>
+      </header>
+      <main>
+        <section className="tabbed">
+          {componentList.map(c => <Tab page={c} />)}
+        </section>
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
